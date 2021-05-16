1fddb9ad847501348007f5fb84e44872dbe66c68
dcguim <dcguim@gmail.com>
2021-05-16 21:18:00 +0200

    add python installation configuration and emacs symbolic link

---
+++ commands.sh
@@ -0,0 +1,2 @@
+port select --set python python38
+port select --set pip pip38
--- emacs-install.sh
+++ emacs-install.sh
@@ -3,5 +3,7 @@ git clone git://git.savannah.gnu.org/emacs.git /Applications/
 cd /Applications/emacs/
 ./configure
 ./make install
-ln -s /Applications/emacs/src/emacs /usr/local/bin/
+ln -s /Applications/emacs/src/emacs /usr/local/bin/emacsclient
 git config --global core.editor "emacs -nw"
+echo -e "emacs(){\nnohup emacsclient \"\$1\" >/dev/null 2>&1 &\n}" >> ~/.zprofile
+source ~/.zprofile

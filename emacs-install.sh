c74720e41b70c7998a3634962905ef8bc72e965a
dcguim <dcguim@gmail.com>
2021-05-17 21:25:21 +0200

    Fix emacs installation, and add virtualenv port

--- emacs-install.sh
+++ emacs-install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-git clone git://git.savannah.gnu.org/emacs.git /Applications/
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this script as root"
+  exit
+fi
+git clone git://git.savannah.gnu.org/emacs.git /Applications/emacs
 cd /Applications/emacs/
+./autogen.sh
 ./configure
+./make
 ./make install
 ln -s /Applications/emacs/src/emacs /usr/local/bin/emacsclient
 git config --global core.editor "emacs -nw"

55c496be6f6e6458a3e46234178cb303fb1c2c49
Daniel Guimaraes <dguim@mbpro.fritz.box>
2022-02-27 20:27:57 +0100

    Force the provision of ZDOTDIR file and invoke few cmds as sudo

--- macports-install.sh
+++ macports-install.sh
@@ -4,13 +4,17 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run this script as root"
   exit
 fi
+if [ -z "$ZDOTDIR" ]
+  then echo "Please provide the directory for the z shell file with ZDOTDIR envvar"
+  exit
+fi
 curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.6.4.tar.bz2
 tar xf MacPorts-2.6.4.tar.bz2
 cd MacPorts-2.6.4/
 ./configure
-make
+sudo make
 sudo make install
-echo -e "# add path for macports binaries\nexport PATH=\"/opt/local/bin/:\$PATH\"" >> $ZDOTDIR/.zshrc
+sudo echo -e "# add path for macports binaries\nexport PATH=\"/opt/local/bin/:\$PATH\"" >> $ZDOTDIR/.zshrc
 source $ZDOTDIR/.zshrc
 port -v selfupdate
 cat macports.txt | sudo xargs port install $1

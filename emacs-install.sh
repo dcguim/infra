d8ac3bd365088ac207466007a85d9af1c925e15c
dcguim <dcguim@gmail.com>
2022-12-12 23:24:51 +0100

    new requirement for newer makeinfo version

--- emacs-install.sh
+++ emacs-install.sh
@@ -11,11 +11,11 @@ git clone git://git.savannah.gnu.org/emacs.git /Applications/emacs
 cd /Applications/emacs/
 ./autogen.sh
 ./configure
-./make
-./make install
+makeinfo=/opt/local/bin/makeinfo make
+makeinfo=/opt/local/bin/makeinfo make install
 ln -s /Applications/emacs/src/emacs /usr/local/bin/emacsserver
 ln -s /Applications/emacs/lib-src/emacsclient /usr/local/bin/emacsclient
 git config --global core.editor "emacs -nw"
-echo -e "emacs(){\nif [ -n \"\$1\" ]; then\n  nohup emacsclient --alternate-editor=emacsserver \"\$1\" >/dev/null 2>&1 &\nelse\n  nohup emacsclient --alternate-editor=emacsserver >/dev/null 2>&1 &\nfi" >> $ZDOTDIR/.zshrc
+echo -e "emacs(){\nif [ -n \"\$1\" ]; then\n  nohup emacsclient --alternate-editor=emacsserver \"\$1\" >/dev/null 2>&1 &\nelse\n  nohup emacsclient --alternate-editor=emacsserver ./ >/dev/null 2>&1 &\nfi" >> $ZDOTDIR/.zshrc
 ln -s $ZDOTDIR/../.emacs ~/.emacs
 source $ZDOTDIR/.zshrc
